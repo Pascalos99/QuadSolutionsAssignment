588f9231ade65d9d571099cb798a7abefd99906e
Pascalos99 <p.anema@student.maastrichtuniversity.nl>
2026-09-06 17:33:21 +0200

    initialize simple Spring-Boot app

---
+++ quad-assignment/src/main/java/com/github/pascalos99/quad_assignment/QuadAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.github.pascalos99.quad_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadAssignmentApplication.class, args);
+	}
+
+}
---
+++ quad-assignment/src/test/java/com/github/pascalos99/quad_assignment/QuadAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pascalos99.quad_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
