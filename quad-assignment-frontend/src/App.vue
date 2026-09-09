0499d4e03d3c5f09fda67c04ac470b1b96e11955
Pascalos99 <p.anema@student.maastrichtuniversity.nl>
2026-09-09 15:16:19 +0200

    Creates a test link to the testing biba page

--- quad-assignment-frontend/src/App.vue
+++ quad-assignment-frontend/src/App.vue
@@ -1,10 +1,11 @@
-<script setup lang="ts"></script>
-
 <template>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
+  </p>
+  <p>
+    There is a test page at <a href="/biba/index.html">biba</a>
   </p>
 </template>
 
