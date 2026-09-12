e65fa78a798e9b0c1e7fe5737eedfeb110ed4fae
Pascalos99 <p.anema@student.maastrichtuniversity.nl>
2026-09-12 23:55:29 +0200

    Implement tests for all TriviaClient methods

--- quad-assignment-backend/src/test/java/com/github/pascalos99/quad_assignment_backend/trivia/TriviaClientTests.java
+++ quad-assignment-backend/src/test/java/com/github/pascalos99/quad_assignment_backend/trivia/TriviaClientTests.java
@@ -17,6 +17,8 @@ import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestClient;
 
 import com.github.pascalos99.quad_assignment_backend.trivia.model.ApiResponse;
+import com.github.pascalos99.quad_assignment_backend.trivia.model.Categories;
+import com.github.pascalos99.quad_assignment_backend.trivia.model.Categories.Category;
 import com.github.pascalos99.quad_assignment_backend.trivia.model.QuestionAndAnswer;
 
 public class TriviaClientTests {
@@ -39,7 +41,7 @@ public class TriviaClientTests {
 	
 	@ParameterizedTest(name = "{index}")
 	@MethodSource("getQuestionsUriCases")
-	void getQuestions_correctlyGeneratesURI(GetQuestionsUriCase tc) {
+	void getQuestions_correctlyGeneratesUri(GetQuestionsUriCase tc) {
 		mockServer.expect(requestTo(tc.uri())).andRespond(withSuccess(EMPTY_RESPONSE, MediaType.APPLICATION_JSON));
 		triviaClient.getQuestions(tc.amount(), tc.category(), tc.difficulty(), tc.type(), tc.encode(), tc.token());
 		mockServer.verify();
@@ -67,6 +69,57 @@ public class TriviaClientTests {
 		ApiResponse response = triviaClient.getQuestions(2, null, null, null, null, null);
 		assertThat(response.response_code()).isOne();
 		assertThat(response.results()).isEmpty();
+	}
+	
+	@Test
+	void getCategories_correctlyGeneratesUriAndDeserializes() throws IOException {
+		String json = new String(getClass().getResourceAsStream("/fixtures/trivia/categories.json").readAllBytes());
+		mockServer.expect(requestTo("http://localhost/api_category.php")).andRespond(withSuccess(json, MediaType.APPLICATION_JSON));
+		Categories response = triviaClient.getCategories();
+		assertThat(response.trivia_categories()).containsExactly(
+				new Category(9, "General Knowledge"),
+				new Category(13, "Entertainment: Musicals & Theatres"),
+				new Category(32, "Entertainment: Cartoon & Animations")
+		);
+		mockServer.verify();
+	}
+	
+	@Test
+	void getToken_correctlyGeneratesUriAndDeserializesSuccess() throws IOException {
+		String json = new String(getClass().getResourceAsStream("/fixtures/trivia/token-request.json").readAllBytes());
+		mockServer.expect(requestTo("http://localhost/api_token.php?command=request")).andRespond(withSuccess(json, MediaType.APPLICATION_JSON));
+		String token = triviaClient.getToken();
+		assertThat(token).isEqualTo("t0k3n");
+		mockServer.verify();
+	}
+	
+	@Test
+	void getToken_correctlyGeneratesUriAndDeserializesFailure() throws IOException {
+		String json = new String(getClass().getResourceAsStream("/fixtures/trivia/token-invalid.json").readAllBytes());
+		mockServer.expect(requestTo("http://localhost/api_token.php?command=request")).andRespond(withSuccess(json, MediaType.APPLICATION_JSON));
+		String token = triviaClient.getToken();
+		assertThat(token).isNull();
+		mockServer.verify();
+	}
+	
+	@Test
+	void resetToken_correctlyGeneratesUriAndDeserializesSuccess() throws IOException {
+		String json = new String(getClass().getResourceAsStream("/fixtures/trivia/token-reset.json").readAllBytes());
+		mockServer.expect(requestTo("http://localhost/api_token.php?command=reset&token=t0k3n"))
+						.andRespond(withSuccess(json, MediaType.APPLICATION_JSON));
+		boolean isValid = triviaClient.resetToken("t0k3n");
+		assertThat(isValid).isTrue();
+		mockServer.verify();
+	}
+	
+	@Test
+	void resetToken_correctlyGeneratesUriAndDeserializesFailure() throws IOException {
+		String json = new String(getClass().getResourceAsStream("/fixtures/trivia/token-invalid.json").readAllBytes());
+		mockServer.expect(requestTo("http://localhost/api_token.php?command=reset&token=t0k3n"))
+						.andRespond(withSuccess(json, MediaType.APPLICATION_JSON));
+		boolean isValid = triviaClient.resetToken("t0k3n");
+		assertThat(isValid).isFalse();
+		mockServer.verify();
 	}
 	
 	static Stream<GetQuestionsUriCase> getQuestionsUriCases() {
