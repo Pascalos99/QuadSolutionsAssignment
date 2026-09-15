a27c0fd4f82c835bb9be2ac6740b0afedcba7a17
Pascalos99 <p.anema@student.maastrichtuniversity.nl>
2026-09-15 14:23:17 +0200

    Reset vue.js project to use JavaScript instead of TypeScript

--- quad-assignment-frontend/vite.config.js
+++ quad-assignment-frontend/vite.config.js
@@ -15,12 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/biba': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
-    }
-  }
 })
