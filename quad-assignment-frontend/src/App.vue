70823bc85496c1cd24ffe2a8ee974081d64096ba
Pascalos99 <p.anema@student.maastrichtuniversity.nl>
2026-09-16 23:47:27 +0200

    Refactor frontend

--- quad-assignment-frontend/src/App.vue
+++ quad-assignment-frontend/src/App.vue
@@ -1,23 +1,13 @@
 
 <script setup>
-  import { ref, onMounted } from 'vue'
+  import { ref, onMounted, computed } from 'vue'
   import { decode}  from 'he'
   import JSConfetti  from 'js-confetti'
 
   const questions = ref(null)
 
-  const do_gaps = ref(true)
-
-  function colorstyle(color) {
-    return `background-color: var(--${color});`
-  }
-
-  const colors = ["red", "green", "blue", "yellow", "gray", "right", "wrong"]
-  const colorstyles = []
-
-  for (let color of colors) {
-    colorstyles.push(colorstyle(color))
-  }
+  const numQuestions = ref(0)
+  const numCorrect = ref(0)
 
   async function fetchData(amount) {
     questions.value = null;
@@ -26,32 +16,17 @@
     );
     questions.value = await res.json()
 
-    // questions.value = questions.value.map((question, i) => 
-    //   ({
-    //     ...question,
-    //     isEven: Boolean(i % 2),
-    //     answers: question.answers.map((text, j) => ({
-    //       ...text,
-    //       styling: colorstyles[j],
-    //       active: true
-    //     }))
-    //   })
-    // );
     questions.value = questions.value.map((question, i) => ({
       ...question,
-      isEven: Boolean(i % 2)
+      isEven: Boolean(i % 2),
+      isDone: false,
+      number: ++numQuestions.value,
+      answers: question.answers.map((answer2, j) => ({
+        text: answer2,
+        state: 'active',
+        position: j
+      }))
     }))
-    for (let question of questions.value) {
-      let res = []
-      for (let i = 0; i < question.answers.length; i++) {
-        res.push({
-          answer: question.answers[i],
-          styling: colorstyles[i],
-          active: true
-        })
-      }
-      question.answers = res;
-    }
   }
 
   async function check(question, answer) {
@@ -68,23 +43,24 @@
   }
 
   async function answerbutton(question, answer) {
-    if (!answer.active) return;
-    let checked = await check(question, answer.answer)
+    if (answer.state != 'active') return;
+    let checked = await check(question, answer.text)
     for (let alt_answer of question.answers) {
-      alt_answer.active = false;
       if (answer == alt_answer) {
         if (checked) {
-          alt_answer.styling = colorstyle("right");
-          showConfetti()
+          alt_answer.state = 'right';
+          numCorrect.value++;
+          showConfetti();
         }
         else {
-          alt_answer.styling = colorstyle("wrong");
+          alt_answer.state = 'wrong'
         }
       }
       else {
-        alt_answer.styling = colorstyle("gray");
+        alt_answer.state = 'inactive'
       }
     }
+    question.isDone = true;
   }
 
   async function loadMore() {
@@ -97,25 +73,35 @@
     confetti.addConfetti()
   }
 
+  const hideCompleted = ref(false)
+
+  const filteredQuestions = computed(() => {
+    return hideCompleted.value
+      ? questions.value.filter((q) => !q.isDone)
+      : questions.value
+  })
+
   onMounted(loadMore)
 </script>
 
 <template>
-  <div class = "content">
+  <div class="topbar">
     <button @click="loadMore">More Questions</button>
-    <button @click="do_gaps ^= true">Switch Layout</button>
+    <button @click="hideCompleted ^= true">{{ hideCompleted?"Show all":"Hide completed" }}</button>
+    {{ numCorrect }}
   </div>
   <p v-if="!questions" class="content">Loading...</p>
   <div v-else class="content">
-    <div class="questions" :class="do_gaps? 'gaps' : 'nogaps'">
-      <div v-for="question in questions" :key="question.uuid" class="question"
-          :class="{ gaps: do_gaps, nogaps: !do_gaps, even: question.isEven, uneven: !question.isEven}">
+    <div class="questions">
+      <div v-for="question in filteredQuestions" :key="question.uuid" class="question"
+          :class="{ even: question.isEven, uneven: !question.isEven}">
         <div class="question-text">{{decode(question.question)}}</div>
         <div class="answers">
-          <button v-for="answer in question.answers" class="answer" :style="answer.styling" @click="answerbutton(question, answer)">
-            <b class="answer-text">{{ decode(answer.answer) }}</b>
+          <button v-for="answer in question.answers" class="answer" :class="answer.state, 'Q'+answer.position" @click="answerbutton(question, answer)">
+            <b class="answer-text">{{ decode(answer.text) }}</b>
           </button>
         </div>
+        <div>{{ question.number }}/{{ numQuestions }}</div>
       </div>
     </div>
   </div>
@@ -128,11 +114,11 @@
   --bg-color2: #6c906b;
   --tx-color-a: #383838;
   --tx-color-q: #000000;
-  --button-border: #619268;
-  --red: #63accb;
-  --green: #F6987E;
-  --blue: #F6C241;
-  --yellow: #EAF32F;
+  --button-border: #62986a;
+  --Q0: #63accb;
+  --Q1: #F6987E;
+  --Q2: #F6C241;
+  --Q3: #EAF32F;
   --gray: #b0b0b0;
   --right: #72cb2a;
   --wrong: #696969;
@@ -149,41 +135,64 @@ button {
 </style>
 
 <style scoped>
+.topbar {
+  display: flex;
+  position: fixed;
+  width: 100%;
+  background-color: var(--body-color);
+  left: 0%;
+  top: 0%;
+  border-color: var(--button-border);
+  border-width: 1mm;
+  border-left: 0;
+  border-right: 0;
+  border-top: 0;
+  border-style: solid;
+  justify-content: center;
+  column-gap: 2.5%;
+  padding: 2mm;
+  padding-right: 0;
+  padding-left: 0;
+  margin-left: 0;
+  margin-right: 0;
+}
 .content {
   display: flex;
   justify-content: center;
-  padding-bottom: 2mm;
   column-gap: 5mm;
+  margin-top: 15mm;
 }
 
 .questions {
   width: clamp(5cm, 95%, 50em);
-}
-.questions.gaps {
   display: grid;
   row-gap: 5mm;
+  justify-content: center;
+  overflow-wrap: break-word;
 }
 
 .question {
+  display: grid;
+  row-gap: 2mm;
   background-color: var(--bg-color);
+  border-radius: 2mm;
   color: var(--tx-color-q);
   text-align: center;
-  padding-bottom: 5mm;
+  padding-bottom: 3mm;
   padding-top: 2.5mm;
-}
-.question.nogaps {
-  padding-bottom: 10mm;
+  overflow-wrap: break-word;
 }
 .question.even {
   background-color: var(--bg-color2);
 }
-
 .question-text {
   padding-left: 5mm;
   padding-right: 5mm;
-  padding-bottom: 2mm;
+  padding-bottom: 1mm;
   font-size: 5.5mm;
+  overflow-wrap: break-word;
 }
+
 .answers {
   display: grid;
   width: 75%;
@@ -200,7 +209,26 @@ button {
   padding-bottom: 0.5em;
   padding-top: 0.5em;
 }
-.answer-text {
-
+/* .answer-text {} */
+.answer.Q0 {
+  background-color: var(--Q0);
+}
+.answer.Q1 {
+  background-color: var(--Q1);
+}
+.answer.Q2 {
+  background-color: var(--Q2);
+}
+.answer.Q3 {
+  background-color: var(--Q3);
+}
+.answer.inactive {
+  background-color: var(--gray);
+}
+.answer.right {
+  background-color: var(--right);
+}
+.answer.wrong {
+  background-color: var(--wrong);
 }
 </style>
